2d0c19a22229f914b6df2734c97263e39ece9276
dxd-future <obuhovsemen797@gmail.com>
2026-04-06 20:15:35 +0900

    начало

--- gui.py
+++ gui.py
@@ -1,3 +1,17 @@
 from tkinter import *
 
-print("ok")
+
+root = Tk()
+
+root.title("Explorer")
+root.iconbitmap(default='icons/папка.ico')
+root.config(bg="#ffffff")
+width = int(root.winfo_screenwidth() * 0.45)
+height = int(root.winfo_screenheight() * 0.45)
+root.geometry(f'{width}x{height}')
+root.resizable(False,False)
+
+
+
+
+root.mainloop()
